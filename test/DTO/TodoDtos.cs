deece0a96133fed0956f399378a5b4308f164e4a
Alessandro <alx.maulu@gmail.com>
2026-05-21 12:34:19 +0200

    Revert "creazione lista todo con crud funzionante"
    This reverts commit a45ab7bbe97cec235e287fa2fa02fd239abe8373.
    push su branch sbagliato

--- test/DTO/TodoDtos.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace test.DTO
-{
-    public record TodoItemDto(int Id, string Title, bool IsCompleted);
-    public record CreateTodoDto(string Title);
-    public record UpdateTodoDto(string Title, bool IsCompleted);
-}
--- test/Endpoints/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace test.Endpoints
-{
-    public class TodoItem
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public bool IsCompleted { get; set; }
-    }
-}
